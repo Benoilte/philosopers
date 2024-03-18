0d013cffea36366d9051b0a2f2190587ebe7ecc7
Bebrandt <benoit.07@hotmail.com>
2024-03-18 19:10:42 +0100

    test philosopher program

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -3,14 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
+/*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 10:31:15 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/03/18 10:32:46 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/03/18 18:47:58 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+
+typedef struct	s_philosopher
+{
+	pthread_t	*philos;
+	int			size;
+}	t_philosophers;
+
+// philo.c
+
+void	*routine();
+
+// ft_atoi.c
+
+int		ft_atoi(const char *str);
 
 #endif
--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
+/*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 10:31:10 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/03/18 10:46:43 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/03/18 19:07:44 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,52 @@
 
 int	main(int argc, char **argv)
 {
+	t_philosophers	*ph;
+	int				i;
+
 	(void)argc;
-	(void)argv;
+	ph = (t_philosophers *)malloc(sizeof(t_philosophers));
+	ph->size = ft_atoi(argv[1]);
+	printf("number of philosopher: %d\n", ph->size);
+	ph->philos = (pthread_t *)malloc(sizeof(pthread_t) * (ph->size));
+	if (!ph->philos)
+		return (1);
+	i = 0;
+	while (i < ph->size)
+	{
+		pthread_create(ph->philos + i, NULL, &routine, NULL);
+		sleep(1);
+		i++;
+	}
+	i = 0;
+	while (i < ph->size)
+	{
+		pthread_join((ph->philos)[i], NULL);
+		i++;
+	}
+	free(ph->philos);
+	return (0);
+}
+
+void	*routine()
+{
+	static int		count;
+
+	printf("Hello, I'm the %d philosoper\n", ++count);
+	// printf("Hello, I'm philo number %d\n", ft_philo_number(ph->philos, ));
+	return (NULL);
+}
+
+int	ft_philo_number(pthread_t *philos, pthread_t philo, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		if ((philos + i) == &philo)
+			return (i + 1);
+		i++;
+	}
 	return (0);
 }

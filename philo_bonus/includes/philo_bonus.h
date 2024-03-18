6b362016778ff20e4a337343977c65e8eea53f86
Bebrandt <benoit.07@hotmail.com>
2024-03-18 19:12:26 +0100

    modify philo_bonus structure

--- philo_bonus/includes/philo_bonus.h
+++ philo_bonus/includes/philo_bonus.h
@@ -3,14 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   philo_bonus.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
+/*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 10:31:22 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/03/18 10:33:08 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/03/18 17:37:57 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_BONUS_H
 # define PHILO_BONUS_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+
+// ft_atoi.c
+
+int	ft_atoi(const char *str);
 
 #endif

d81e09d8d76ce9d13e416b0284224c785b601f76
benoit brandt <benoit.07@hotmail.com>
2024-03-19 18:27:01 +0100

    create logic in main to check number of argument and check if all argument are numbers

--- philo/srcs/ft_isdigit.c
+++ philo/srcs/ft_isdigit.c
@@ -1,40 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/18 10:31:15 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/03/19 18:14:25 by bebrandt         ###   ########.fr       */
+/*   Created: 2024/03/19 18:09:19 by bebrandt          #+#    #+#             */
+/*   Updated: 2024/03/19 18:13:36 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
+#include "philo.h"
 
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <pthread.h>
-
-typedef struct	s_philosopher
+/*
+Return non-zero value if 'c' is in 0-9
+*/
+int	ft_isdigit(int c)
 {
-	pthread_t	*philos;
-	int			size;
-}	t_philosophers;
+	return (c >= '0' && c <= '9');
+}
 
-// philo.c
-
-int		args_are_wrong(int argc, char **argv);
-void	*routine(void *num);
-
-// ft_atoi.c
-
-int		ft_atoi(const char *str);
-
-// ft_isdigit.c
-
-int		ft_isdigit(int c);
-
-#endif
